783a5aa21e29ad021acc9378bfd21123735e2b36
JR:beaglebone <jonreal731@gmail.com>
2015-11-16 17:06:16 -0800

    Lot's of stuff, tui plus motor stuff.

--- anklebotOS/include/adcdriver.h
+++ anklebotOS/include/adcdriver.h
@@ -3,6 +3,7 @@
 
 #define MUX_SEL_PIN     7
 
+
 /* --- Digital output reg. */
 volatile register uint32_t __R30;
 
@@ -11,7 +12,8 @@ extern volatile uint32_t *debugBuffer;
 
 /* ---- Prototypes ---- */
 void adcInit(void);
-void adcSample(volatile uint16_t adc[8]);
+void adcSample_1(volatile uint16_t adc[8]);
+void adcSample_2(volatile uint16_t adc[8]);
 void adcCleanUp(void);
 
 #endif
