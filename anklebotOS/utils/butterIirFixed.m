dd77ba743a545541de14dadaecb7d8b2ff6533a3
JR:alphaPlus <jonreal731@gmail.com>
2016-02-04 15:05:18 -0800

    Filter coeff.

--- anklebotOS/utils/butterIirFixed.m
+++ anklebotOS/utils/butterIirFixed.m
@@ -1,10 +1,23 @@
-function rtn = butterIirFixed(n,Wc,fileName)
+function butterIirFixed(n,Wc,fileName)
 
   % Calculate coefficients
-  [b,a] = butter(n,Wc)
+  [b,a] = butter(n,Wc,'low')
 
-  % Shift coefficients (ie multiply by 32768)
-  b = bitsll(b,15)
-  a = bitsll(a,15)
+  % Multiply by 32768 and round
+  b = round(b.*32768)
+  a = round(a.*32768)
 
+  % Write to file
+  if exist(fileName,'file')
+    fprtinf('\tFile already exsists,');
+    usr_val = input('overwrite? [y|n]','s');
+    if strcmp(usr_val,'n')
+      return;
+    end
+  end
+
+  fid = fopen(fileName,'w');
+  fprintf(fid,'%d\n',n);
+  fprintf(fid,'%d\n',b);
+  fprintf(fid,'%d\n',a);
 end
