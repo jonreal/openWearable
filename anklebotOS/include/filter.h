68aec3f5c375f14b525422dc46637dce9e1fa9d8
JR:beaglebone <jonreal731@gmail.com>
2016-02-04 16:03:29 -0800

    Data

--- anklebotOS/include/filter.h
+++ anklebotOS/include/filter.h
@@ -6,5 +6,6 @@
 
 void firFixedInit(volatile int16_t *insamp);
 int16_t firFixed(int16_t *coeffs, volatile int16_t *insamp, int16_t input);
-
+int16_t iirFixedPoint(int16_t N, int16_t *b, int16_t *a,
+                      int16_t *x, int16_t *y, int16_t s);
 #endif
