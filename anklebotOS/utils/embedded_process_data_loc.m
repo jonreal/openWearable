2043e4910abf4971b3e23112f5c4aa8cd50a48b8
JR:alphaPlus <jonreal731@gmail.com>
2016-02-21 19:42:25 -0800

    MAtlab

--- anklebotOS/utils/embedded_process_data_loc.m
+++ anklebotOS/utils/embedded_process_data_loc.m
@@ -1,148 +1,176 @@
 function rtn = embedded_process_data(trialName)
+  lineCnt = 0;
 
-  % Check file
+  % Check file exists
   file = ['./',trialName];
   if exist(file,'file') ~= 2
     fprintf('\n\tEmbedded file not found\n');
     rtn = []
+    return
   end
 
-  D = dlmread(file,'\t',3,0);
+  fid = fopen(file);
 
-  % TODO: params
+  % First line is date and time
+  rtn.date = fscanf(fid,'#Date: %s',1);
+  rtn.time = fscanf(fid,'%s\n',1);
+  lineCnt = lineCnt + 1;
 
-  % Time Step (TODO: read in)
-  rtn.freq = 1000;
+  % Skip 2 lines
+  tline = fgetl(fid);
+  lineCnt = lineCnt + 1;
+  tline = fgetl(fid);
+  lineCnt = lineCnt + 1;
 
-  % Syncing (Remove unsynced data)
+  % Memory
+  rtn.memalloc.params = fscanf(fid,'#\tParameter memory: %i bytes.\n',1);
+  lineCnt = lineCnt + 1;
+
+  rtn.memalloc.lookup = fscanf(fid,'#\tLookup table memory: %i bytes.\n',1);
+  lineCnt = lineCnt + 1;
+
+  rtn.memalloc.data = fscanf(fid,'#\tData memory: %i bytes.\n',1);
+  lineCnt = lineCnt + 1;
+
+  rtn.memalloc.units = 'bytes';
+
+  % Skip 2 lines
+  tline = fgetl(fid);
+  lineCnt = lineCnt + 1;
+  tline = fgetl(fid);
+  lineCnt = lineCnt + 1;
+
+  % Paramters
+  rtn.params.frq = fscanf(fid,'#\tFrq = %i (Hz)\n',1);
+  lineCnt = lineCnt + 1;
+
+  rtn.params.frq_ticks = fscanf(fid,'#\tTicks = %i\n',1);
+  lineCnt = lineCnt + 1;
+
+  rtn.params.Kp = fscanf(fid,'#\tKp = %f\n',1);
+  lineCnt = lineCnt + 1;
+
+  rtn.params.Kd = fscanf(fid,'#\tKd = %f\n',1);
+  lineCnt = lineCnt + 1;
+
+  rtn.params.pos0 = fscanf(fid,'#\tanklePos0 = %f\n',1);
+  lineCnt = lineCnt + 1;
+
+  rtn.params.l_forceThrs = fscanf(fid,'#\tl_forceThrs = %f\n',1);
+  lineCnt = lineCnt + 1;
+
+  rtn.params.l_d_forceThrs = fscanf(fid,'#\tl_d_forceThrs = %f\n',1);
+  lineCnt = lineCnt + 1;
+
+  rtn.params.r_forceThrs = fscanf(fid,'#\tr_forceThrs = %f\n',1);
+  lineCnt = lineCnt + 1;
+
+  rtn.params.r_d_forceThrs = fscanf(fid,'#\tr_d_forceThrs = %f\n',1);
+  lineCnt = lineCnt + 1;
+
+  % Skip 1 line
+  tline = fgetl(fid);
+  lineCnt = lineCnt + 1;
+
+  % Filter Order
+  rtn.filt.N = fscanf(fid,'#Filter Coefficients (N = %i):\n',1);
+  lineCnt = lineCnt + 1;
+  for i=0:rtn.filt.N
+    rtn.filt.b(i+1) = fscanf(fid,['#\tb[', num2str(i) ,'] : %f '], 1);
+    rtn.filt.a(i+1) = fscanf(fid,['a[', num2str(i) ,'] : %f\n'],1);
+    lineCnt = lineCnt + 1;
+  end
+
+  % Skip 1 line
+  tline = fgetl(fid);
+  lineCnt = lineCnt + 1;
+
+  % Headers
+  tline = fgetl(fid);
+  lineCnt = lineCnt + 1;
+  labels = textscan(tline, '%s');
+
+  % First label is '#', remove it.
+  labels = labels{1};
+  labels(1) = [];
+
+  % Close file
+  fclose(fid);
+
+  % Read data
+  D = dlmread(file, '', lineCnt, 0);
+
+  [m,n] = size(D);
+  nn = numel(labels);
+
+  if nn ~= n
+    [nn, n]
+    error('Labels/column mismatch!');
+    return
+  end
+
+  % Remove unsynced data
   D(D(:,2) ~= 1,:) = [];
 
-  rtn.timeStamp = D(:,1);
-  rtn.time = (D(:,1) - D(1,1)).*1/1000;
+  % Sort data based on frameID (data isn't guaranteed to arrive in order)
+  % Also buffer maybe skipped (ie. hole in data)
+  [values, order] = sort(D(:,1));
+  D = D(order,:);
 
-  rtn.sync = D(:,2);
-  rtn.avgT_cnts = D(:,3);
-  rtn.heelstrikeCnt = D(:,4);
-  rtn.gaitPhase = D(:,5);
-  rtn.anklePos = D(:,6);
-  rtn.ankleVel = D(:,7);
-  rtn.fbCurrentCmd = D(:,8);
-  rtn.ffCurrentCmd = D(:,9);
-  rtn.motorDuty = D(:,10);
-  rtn.motorCurrent = D(:,11);
-  rtn.motorVel = D(:,12);
-  rtn.amp1.s1 = D(:,13);
-  rtn.amp1.s2 = D(:,14);
-  rtn.amp1.s3 = D(:,15);
-  rtn.amp2.s1 = D(:,16);
-  rtn.amp2.s2 = D(:,17);
-  rtn.amp2.s3 = D(:,18);
-  rtn.acc1 = D(:,19);
-  rtn.acc2 = D(:,20);
-  rtn.acc3 = D(:,21);
-  rtn.gyro1 = D(:,22);
-  rtn.gyro2 = D(:,23);
-  rtn.gyro3 = D(:,24);
+  figure;
+    plot(D(:,1))
+    xlabel('Data Point','fontsize',20)
+    ylabel('Time Stamp','fontsize',20)
 
-  fs = rtn.freq;
-  fc = 13;
+  figure;
+    plot(D(1:end-1,1),D(2:end,1) - D(1:end-1,1))
+    xlabel('Data Point','fontsize',20)
+    ylabel('Stamp[k+1] - Stamp[k]','fontsize',20)
 
-  % HS algothrim (TODO: excecute on BBB)
-%  [b,a] = butter(2,2*fc/fs);
-%
-%  time = rtn.time;
-%  l_filt = filter(b,a,rtn.amp1.s3);
-%  r_filt = filter(b,a,rtn.amp2.s3);
-%
-%  r_tol1 = 1050;
-%  r_tol2 = 200;
-%  r_isHeelPushed = 0.*r_filt;
-%  r_vel = 0.*r_filt;
-%  r_hs_index = [];
-%
-%  l_tol1 = 900;
-%  l_tol2 = 450;
-%  l_isHeelPushed = 0.*l_filt;
-%  l_vel = 0.*l_filt;
-%  l_hs_index = [];
-%
-%  for i=1:numel(time)
-%    if i<2
-%      continue
-%    end
-%
-%    r_vel(i) = 2*rtn.freq*r_filt(i) - 2*rtn.freq*r_filt(i-1) - r_vel(i-1);
-%    l_vel(i) = 2*rtn.freq*l_filt(i) - 2*rtn.freq*l_filt(i-1) - l_vel(i-1);
-%
-%    % Right HS detection
-%    if r_isHeelPushed(i-1) == 0
-%      if (r_vel(i) > r_tol1) && (r_filt(i) < r_tol2)
-%        r_isHeelPushed(i) = 1;
-%        r_hs_index(end+1) = i;
-%      else
-%        r_isHeelPushed(i) = 0;
-%      end
-%    elseif r_isHeelPushed(i-1) == 1
-%      if (r_vel(i) < -r_tol1) && (r_filt(i) > r_tol2)
-%        r_isHeelPushed(i) = 0;
-%      else
-%        r_isHeelPushed(i) = 1;
-%      end
-%    end
-%
-%    % Left HS detection
-%    if l_isHeelPushed(i-1) == 0
-%      if (l_vel(i) > l_tol1) && (l_filt(i) < l_tol2)
-%        l_isHeelPushed(i) = 1;
-%        l_hs_index(end+1) = i;
-%      else
-%        l_isHeelPushed(i) = 0;
-%      end
-%    elseif l_isHeelPushed(i-1) == 1
-%      if (l_vel(i) < -l_tol1) && (l_filt(i) > l_tol2)
-%        l_isHeelPushed(i) = 0;
-%      else
-%        l_isHeelPushed(i) = 1;
-%      end
-%    end
-%  end
-%
-%  % Remove first and last
-%  l_hs_index(1) = [];
-%  l_hs_index(end) = [];
-%  r_hs_index(1) = [];
-%  r_hs_index(end) = [];
-%
-%  rtn.gaitEvents.l.hs_index = l_hs_index(:);
-%  rtn.gaitEvents.r.hs_index = r_hs_index(:)';
-%  rtn.gaitEvents.l.hs_time = rtn.time(l_hs_index);
-%  rtn.gaitEvents.r.hs_time = rtn.time(r_hs_index);
-%
-%  figure; hold all
-%    subplot(311); hold all
-%      plot(time,rtn.amp2.s3,'k')
-%      plot(time,r_filt,'y')
-%      plot(time,r_filt.*0 + r_tol2);
-%      title('Right','fontsize',20)
-%    subplot(312); hold all
-%      plot(time,r_vel,'r')
-%      plot(time,r_vel.*0 + r_tol1);
-%      plot(time,r_vel.*0 - r_tol1);
-%    subplot(313); hold all
-%      plot(time,r_filt,'y')
-%      plot(time,r_isHeelPushed.*max(r_filt),'r')
-%
-%  figure; hold all
-%    subplot(311); hold all
-%      plot(time,rtn.amp1.s3,'k')
-%      plot(time,l_filt,'y')
-%      plot(time,l_filt.*0 + l_tol2);
-%      title('Left','fontsize',20)
-%    subplot(312); hold all
-%      plot(time,l_vel,'r')
-%      plot(time,l_vel.*0 + l_tol1);
-%      plot(time,l_vel.*0 - l_tol1);
-%    subplot(313); hold all
-%      plot(time,l_filt,'y')
-%      plot(time,l_isHeelPushed.*max(l_filt),'r')
+  % Calculate time vector
+  rtn.data.time = (D(:,1) - D(1,1)).*(1/rtn.params.frq);
+
+  % Store data in struct
+  for i=1:n
+    rtn.data.(labels{i}) = D(:,i);
+  end
+
+  % Remove repeated hs
+  l_hs = unique(rtn.data.l_hs);
+  r_hs = unique(rtn.data.r_hs);
+  l_hs(l_hs < rtn.data.frame(1)) = [];
+  r_hs(r_hs < rtn.data.frame(1)) = [];
+
+  r_ii = [];
+  % Find hs indice w.r.t frame ID
+  for i=1:numel(r_hs)
+    [~,ii] = min(abs(rtn.data.frame - r_hs(i)));
+    r_ii(i) = ii;
+  end
+  l_ii = [];
+  for i=1:numel(l_hs)
+    [~,ii] = min(abs(rtn.data.frame - l_hs(i)));
+    l_ii(i) = ii;
+  end
+
+  % Last
+  if (numel(r_ii) > 2)
+    r_ii(end) = [];
+  else
+    r_ii = [];
+  end
+
+  % Remove first and last
+  if (numel(l_ii) > 2)
+    l_ii(end) = [];
+  else
+    l_ii = [];
+  end
+
+  rtn.gaitEvents.l.hs_index = l_ii;
+  rtn.gaitEvents.r.hs_index = r_ii;
+  % Convert to time
+  rtn.gaitEvents.l.hs_time = rtn.data.time(l_ii);
+  rtn.gaitEvents.r.hs_time = rtn.data.time(r_ii);
+
 end
