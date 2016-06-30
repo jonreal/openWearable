bf27803479778ee069c655011ac4b47814cc9162
JR:alphaPlus <jonreal731@gmail.com>
2016-06-30 14:48:17 -0700

    Update matlab utils.
    Former-commit-id: 6bfedf0df00b4585a41103ee8e2975bf7b0c6c83

--- anklebotOS/utils/response.m
+++ anklebotOS/utils/response.m
@@ -4,6 +4,7 @@ function rtn = response(trialName);
 
   figure;
     subplot(211); hold all;
+      title('Left Heel Sensor','fontsize',20);
       plot(rtn.data.time, rtn.data.l_s3,'k')
       plot(rtn.data.time, rtn.data.l_s3.*0 + rtn.params.l_forceThrs);
       plot(rtn.data.time, rtn.data.l_gp.*max(abs(rtn.data.l_s3)));
@@ -11,6 +12,7 @@ function rtn = response(trialName);
            rtn.data.l_s3(rtn.gaitEvents.l.hs_index),'or');
       grid on
     subplot(212); hold all;
+      title('Left Heel Sensor Derivative','fontsize',20);
       plot(rtn.data.time, rtn.data.l_d_s3,'k');
       plot(rtn.data.time, rtn.data.l_d_s3.*0 + rtn.params.l_d_forceThrs);
       plot(rtn.data.time, rtn.data.l_d_s3.*0 - rtn.params.l_d_forceThrs);
@@ -20,6 +22,7 @@ function rtn = response(trialName);
 
   figure;
     subplot(211); hold all;
+      title('Right Heel Sensor','fontsize',20);
       plot(rtn.data.time, rtn.data.r_s3,'k')
       plot(rtn.data.time, rtn.data.r_s3.*0 + rtn.params.r_forceThrs);
       plot(rtn.data.time, rtn.data.r_gp.*max(abs(rtn.data.r_s3)));
@@ -27,6 +30,7 @@ function rtn = response(trialName);
            rtn.data.r_s3(rtn.gaitEvents.r.hs_index),'or');
       grid on
     subplot(212); hold all;
+      title('Right Heel Sensor Derivative','fontsize',20);
       plot(rtn.data.time, rtn.data.r_d_s3,'k');
       plot(rtn.data.time, rtn.data.r_d_s3.*0 + rtn.params.r_d_forceThrs);
       plot(rtn.data.time, rtn.data.r_d_s3.*0 - rtn.params.r_d_forceThrs);
@@ -36,20 +40,24 @@ function rtn = response(trialName);
 
   figure;
     subplot(211); hold all;
-    plot(rtn.data.time, rtn.data.u_ff + rtn.data.u_fb);
-    plot(rtn.data.time, bits2Amps(rtn.data.mtrCurr));
-    plot(rtn.data.time, bits2Amps(rtn.data.mtrVel));
+      title('Motor Current','fontsize',20);
+      plot(rtn.data.time, bits2Amps(rtn.data.mtrCurr_d));
+      plot(rtn.data.time, rtn.data.u_ff + rtn.data.u_fb);
+      plot(rtn.data.time, bits2Amps(rtn.data.mtrCurr));
+      legend('Command','Command','Actual')
+
     subplot(212); hold all;
-    plot(rtn.data.time, rtn.data.duty)
+      title('PWM Duty','fontsize',20);
+      plot(rtn.data.time, rtn.data.duty)
 
 end
 
-function rtn = bits2Amps(cur)
+function rtn = bits2Amps(current_in_bits)
 
   % Linear fit
-  rtn = 0.00733 .* cur - 15;
-
-  % Flip sign to match convention
-  rtn = -rtn;
+  %
+  % m = (20 A - (-20 A) / 2^12 bits = 0.0098
+  %
+  rtn = -0.0098 .* current_in_bits + 20;
 
 end
