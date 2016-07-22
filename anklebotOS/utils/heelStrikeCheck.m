0806f0937d16c48b834e829c07d1b9fec4a6c651
JR:alphaPlus <jonreal731@gmail.com>
2016-07-22 14:09:38 -0700

    Updated utils
    Former-commit-id: ef709e614fb9e268df02366b29080badd41a6b88

--- anklebotOS/utils/heelStrikeCheck.m
+++ anklebotOS/utils/heelStrikeCheck.m
@@ -1,15 +1,16 @@
 function rtn = heelStrikeCheck(trialName)
 
-  rtn = embedded_process_data_loc(trialName);
+  rtn = embedded_process_data(trialName);
 
   figure; hold all;
     subplot(211); hold all;
       plot(rtn.data.time,rtn.data.l_s3,'-k');
       plot(rtn.data.time,rtn.data.time.*0 + rtn.params.l_forceThrs,'--r')
-      for i=1:numel(rtn.segmentedGaitCycles.l_time(:,1))
-        plot(rtn.segmentedGaitCycles.l_time(i,1),0,'og')
-        plot(rtn.segmentedGaitCycles.l_time(i,2),100,'or')
+      for i=1:numel(rtn.segmentedGaitCycles.l.time(:,1))
+        plot(rtn.segmentedGaitCycles.l.time(i,1),0,'og')
+        plot(rtn.segmentedGaitCycles.l.time(i,2),100,'or')
       end
+      plot(rtn.data.time,rtn.data.l_gp.*max(rtn.data.l_s3),'--g')
       title('Left heel sensor','fontsize',20)
       xlabel('Time (s)','fontsize',20);
       ylabel('Bits','fontsize',20);
@@ -18,26 +19,35 @@ function rtn = heelStrikeCheck(trialName)
     subplot(212); hold all;
       plot(rtn.data.time,rtn.data.l_d_s3,'-k');
       plot(rtn.data.time,rtn.data.time.*0 + rtn.params.l_d_forceThrs,'--r');
+      plot(rtn.data.time,rtn.data.time.*0 - rtn.params.l_d_forceThrs,'--r');
       title('Derivative','fontsize',20)
       xlabel('Time (s)','fontsize',20);
       ylabel('Bits/s','fontsize',20);
       grid on; box on;
 
-  return
-
   figure; hold all;
     subplot(211); hold all;
       plot(rtn.data.time,rtn.data.r_s3,'-k');
       plot(rtn.data.time,rtn.data.time.*0 + rtn.params.r_forceThrs,'--r')
-      title('Right heel sensor','fontsize',20)
+      for i=1:numel(rtn.segmentedGaitCycles.r.time(:,1))
+        plot(rtn.segmentedGaitCycles.r.time(i,1),0,'og')
+        plot(rtn.segmentedGaitCycles.r.time(i,2),100,'or')
+      end
+      plot(rtn.data.time,rtn.data.r_gp.*max(rtn.data.r_s3),'--g')
+      title('Left heel sensor','fontsize',20)
       xlabel('Time (s)','fontsize',20);
       ylabel('Bits','fontsize',20);
+      grid on; box on;
+
     subplot(212); hold all;
       plot(rtn.data.time,rtn.data.r_d_s3,'-k');
-      plot(rtn.data.time,rtn.data.time.*0 + rtn.params.r_d_forceThrs,'--r')
+      plot(rtn.data.time,rtn.data.time.*0 + rtn.params.r_d_forceThrs,'--r');
+      plot(rtn.data.time,rtn.data.time.*0 - rtn.params.r_d_forceThrs,'--r');
       title('Derivative','fontsize',20)
       xlabel('Time (s)','fontsize',20);
       ylabel('Bits/s','fontsize',20);
+      grid on; box on;
+
 end
 
 
