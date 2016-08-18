a339270be13f6280ede94db3d61e220e8a26117f
JR:alphaPlus <jonreal731@gmail.com>
2016-08-18 14:22:53 -0700

    Edits in utils

--- anklebotOS/utils/response.m
+++ anklebotOS/utils/response.m
@@ -52,12 +52,4 @@ function rtn = response(trialName);
 
 end
 
-function rtn = bits2Amps(current_in_bits)
 
-  % Linear fit
-  %
-  % m = (20 A - (-20 A) / 2^12 bits = 0.0098
-  %
-  rtn = -0.0098 .* current_in_bits + 20;
-
-end
