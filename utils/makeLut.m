80c7f6be223cd430b1fe91a399b799679e77d3af
JR:alphaPlus <jonreal731@gmail.com>
2016-10-16 17:45:29 -0700

    makeLut

---
+++ utils/makeLut.m
@@ -0,0 +1,39 @@
+function makeLut(func,varargin)
+
+  nVarargin = length(varargin);
+
+  if ((nVarargin == 3) && strCmp(varargin{1},'filename'))
+    filename = varargin{2};
+  else
+    filename = 'lut';
+  end
+
+
+  x = 0:0.1:(100-0.1);
+  y = func(x);
+
+  figure;
+    plot(x,y,'k')
+    ylabel('current (A)','fontsize',20);
+    xlabel('% gait','fontsize',20);
+
+  writeFlag = 1;
+  file = ['../app/config/','filename'];
+  if exist(file,'file') == 2
+    fprintf('\n\t\tFile already exist');
+    usr_input = input(' overwrite? [y/n]','s');
+    if strcmp('y',usr_input)
+      writeFlag = 1;
+    else
+      writeFlag = 0;
+      fprintf('\n\tSignal not written to file.\n');
+    end
+  end
+  if writeFlag
+    fid = fopen(file,'w');
+    fprintf(fid,'%f\n',y);
+    fclose(fid);
+  end
+
+
+end
