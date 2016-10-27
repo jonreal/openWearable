a49372fde4f900a47505d0dde13cd1083c59afad
JR:beaglebone1 <jonreal731@gmail.com>
2016-10-27 11:00:41 -0700

    Fixed bits2Rpm

--- utils/bits2Rpm.m
+++ utils/bits2Rpm.m
@@ -3,13 +3,13 @@ function rtn = bits2Rpms(velocity_in_bits)
 %
 % Opposite convention as motor drive
 
-% 0 bits (0 Volts)      <--> -20000 rpm
-% 2^12 bits (1.8 volts) <--> 20000 rpm
+% 0 bits (0 Volts)      <--> -40000 rpm
+% 2^12 bits (1.8 volts) <--> 40000 rpm
 %
 
   % negative sign for opposite sign convention
-  m = - (20000 - (-20000)) / 2^12;
-  b = - (-20000);
+  m = - (40000 - (-40000)) / 2^12;
+  b = - (-40000);
 
   rtn = m.* velocity_in_bits + b;
 
