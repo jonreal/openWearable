67cd3ec23d7efd1b084c7ef0c62ff596e97e38d4
JR:alphaPlus <jonreal731@gmail.com>
2017-07-04 17:17:56 -0700

    Still preparing

--- utils/bits2Rpm.m
+++ utils/bits2Rpm.m
@@ -3,13 +3,16 @@ function rtn = bits2Rpms(velocity_in_bits)
 %
 % Opposite convention as motor drive
 
-% 0 bits (0 Volts)      <--> -60000 rpm
-% 2^12 bits (1.8 volts) <--> 60000 rpm
+maxVel = 80000;
+
+% 0 bits (0 Volts)      <--> -max Vel rpm
+% 2^12 bits (1.8 volts) <--> max Vel rpm
 %
+  
 
   % negative sign for opposite sign convention
-  m = - (60000 - (-60000)) / 2^12;
-  b = - (-60000);
+  m = - (maxVel - (-maxVel)) / 2^12;
+  b = - (-maxVel);
 
   rtn = m.* velocity_in_bits + b;
 
--- utils/heelStrikeCheck.m
+++ utils/heelStrikeCheck.m
@@ -6,10 +6,10 @@ function rtn = heelStrikeCheck(trialName)
     subplot(211); hold all;
       plot(rtn.data.time,rtn.data.l_s3,'-k');
       plot(rtn.data.time,rtn.data.time.*0 + rtn.params.l_forceThrs,'--r')
-      for i=1:numel(rtn.segmentedGaitCycles.l.time(:,1))
-        plot(rtn.segmentedGaitCycles.l.time(i,1),0,'og')
-        plot(rtn.segmentedGaitCycles.l.time(i,2),100,'or')
-      end
+ %     for i=1:numel(rtn.segmentedGaitCycles.l.time(:,1))
+ %       plot(rtn.segmentedGaitCycles.l.time(i,1),0,'og')
+ %       plot(rtn.segmentedGaitCycles.l.time(i,2),100,'or')
+ %     end
       plot(rtn.data.time,rtn.data.l_gp.*max(rtn.data.l_s3),'--b')
       title('Left heel sensor','fontsize',20)
       xlabel('Time (s)','fontsize',20);
@@ -29,10 +29,10 @@ function rtn = heelStrikeCheck(trialName)
     subplot(211); hold all;
       plot(rtn.data.time,rtn.data.r_s3,'-k');
       plot(rtn.data.time,rtn.data.time.*0 + rtn.params.r_forceThrs,'--r')
-      for i=1:numel(rtn.segmentedGaitCycles.r.time(:,1))
-        plot(rtn.segmentedGaitCycles.r.time(i,1),0,'og')
-        plot(rtn.segmentedGaitCycles.r.time(i,2),100,'or')
-      end
+%      for i=1:numel(rtn.segmentedGaitCycles.r.time(:,1))
+%        plot(rtn.segmentedGaitCycles.r.time(i,1),0,'og')
+%        plot(rtn.segmentedGaitCycles.r.time(i,2),100,'or')
+%      end
       plot(rtn.data.time,rtn.data.r_gp.*max(rtn.data.r_s3),'--b')
       title('Left heel sensor','fontsize',20)
       xlabel('Time (s)','fontsize',20);
