b3efb0eedafb94cbdafbff40d830de2448cafad5
JR:LabRat <jdrealmuto@gmail.com>
2019-01-08 15:22:39 -0800

    Proto for bayes filter with decimation

--- utils/checkFiltTest.m
+++ utils/checkFiltTest.m
@@ -24,21 +24,18 @@ end
 
 
 % Nonlinear Bayes Filter
-n = 100;
-alpha = 0.001;
-beta = 0.01;
+n = 32;
+alpha = 0.1;
+beta = 0.1;
 maxEmg = 1;
 scale = 4;
 debug = 0;
-
-kappa = alpha*(1/fs)/(1/n)^2
-eta = beta*(1/fs)
+div = 10;
 
 prior = ones(n,1)*1000/n;
 posterior = zeros(n,N);
 MAP = zeros(N,1);
 
-prior_ = ones(n,1)/n;
 posterior_ = zeros(n,N);
 MAP_ = zeros(N,1);
 
@@ -51,9 +48,14 @@ emg = D(:,1);
 %emg = scale*emg*maxEmg;
 %emg(emg>maxEmg) = maxEmg;
 
-prior
+f = initBayes('n',n, ...
+              'alpha',alpha, ...
+              'beta',beta, ...
+              'maxEmg',maxEmg, ...
+              'debug',debug, ...
+              'div',div);
 
-%N=1000;
+%N = 2;
 
 for i=1:N
 
@@ -79,13 +81,16 @@ for i=1:N
                               'debug',debug);
   prior = posterior(:,i);
 
-  [posterior_(:,i), MAP_(i)] = nlb_sanger_clone(prior_, sample, ...
-                              'fs',fs, ...
-                              'n', n, ...
-                              'alpha',alpha, ...
-                              'beta',beta, ...
-                              'maxEmg',maxEmg);
-  prior_ = posterior_(:,i);
+  [posterior_(:,i), MAP_(i), f] = nonlinBayesFilt_div(f, sample);
+
+
+  %[posterior_(:,i), MAP_(i)] = nlb_sanger_clone(prior_, sample, ...
+  %                            'fs',fs, ...
+  %                            'n', n, ...
+  %                            'alpha',alpha, ...
+  %                            'beta',beta, ...
+  %                            'maxEmg',maxEmg);
+  %prior_ = posterior_(:,i);
 
   samples(i) = sample;
 end
@@ -97,6 +102,6 @@ figure; hold all;
 %    plot(t(1:N),M(1:N,2),'--r');
   subplot(313); hold all;
     plot(t(1:N),MAP(1:N),'k');
-    plot(t(1:N),MAP_(1:N),'--g');
-    plot(t(1:N),M(1:N,3),'r');
+    plot(t(1:N),MAP_(1:N),'r');
+%    plot(t(1:N),M(1:N,3),'r');
 
