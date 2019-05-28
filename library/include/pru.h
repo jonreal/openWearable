40e9726609e55bd4b56d6b63d50d8af0d51a517c
JR:beaglebone <jonreal731@gmail.com>
2019-05-28 16:26:16 +0000

    Added device tree

--- library/include/pru.h
+++ library/include/pru.h
@@ -12,21 +12,10 @@
 #define PRU_SHARED_DRAM		(PRU_GLOBAL_BASE + 0x00010000)
 
 // remote proc driver paths
-#define PRU_UNBIND "/sys/bus/platform/drivers/pru-rproc/unbind"
-#define PRU_BIND "/sys/bus/platform/drivers/pru-rproc/bind"
-#define PRU0_NAME "4a334000.pru0"
-#define PRU1_NAME "4a338000.pru1"
-#define PRU_NAME_LEN 13
-#define PRU0_UEVENT "/sys/bus/platform/drivers/pru-rproc/4a334000.pru0/uevent"
-#define PRU1_UEVENT "/sys/bus/platform/drivers/pru-rproc/4a338000.pru1/uevent"
-
-
-// 
 #define PRU0_FW "/sys/class/remoteproc/remoteproc1/firmware"
 #define PRU1_FW "/sys/class/remoteproc/remoteproc2/firmware"
 #define PRU0_STATE "/sys/class/remoteproc/remoteproc1/state"
 #define PRU1_STATE "/sys/class/remoteproc/remoteproc2/state"
-
 
 int PruMemMap(pru_mem_t* pru_mem);
 int PruInit(void);
