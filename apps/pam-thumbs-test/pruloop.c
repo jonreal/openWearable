9aac48162cb5a89289f53acfc4045f32f4133194
JR:beaglebone <jonreal731@gmail.com>
2019-07-23 21:07:51 +0000

    New device tree for pinmuxing beagle blue

--- apps/pam-thumbs-test/pruloop.c
+++ apps/pam-thumbs-test/pruloop.c
@@ -15,46 +15,40 @@
 
 #include "pruloop.h"
 #include "thumbsup.h"
+#include "sync.h"
 #include "filtcoeff.h"
 
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
 
-
 // Pam stuff
 i2cmux_t* mux;
-
 reservoir_t* reservoir;
 pam_t* pam1;
 pam_t* pam2;
 const uint32_t refract_cnts = 250;
-const fix16_t p_default = 0xF0000; // 10 psi
 
 // Thumbs up stuff
 thumbs_up_interaction_t* thumbsup;
-const fix16_t mv_alpha = 0x21;
-const fix16_t pro_thrshld = fix16_one;
-const fix16_t sup_thrshld = -fix16_one;
-
 iir_filt_t* mvavg;
 const fix16_t b_mvavg[2] = {0x21, 0};                    // [0.0005, 0]
 const fix16_t a_mvavg[2] = {fix16_one, 0xFFFF0021};      // [1 -(1-0.0005)]
 
-// Tracking
-const uint32_t Td = 5000;
-uint32_t t0, t;
-
+// Sync stuff
+sync_t* sync;
+const uint8_t sync_pin = 11;
 // ---------------------------------------------------------------------------
 // PRU0
 //
 // Edit user defined functions below
 // ---------------------------------------------------------------------------
 void Pru0Init(pru_mem_t* mem) {
-
-
-  // init params
-  mem->p->pd = p_default;
-  t0 = 0;
+  mem->p->p_sense = 0xF0000;    // 15 psi
+  mem->p->p_pro = 0x280000;     // 40 psi
+  mem->p->p_sup = 0x280000;     // 40 psi
+  mem->p->thr_sup = -fix16_one;
+  mem->p->thr_pro = fix16_one;
+  mem->p->hold_cnt = 3000;
 }
 
 void Pru0UpdateState(const pru_count_t* c,
@@ -88,14 +82,17 @@ void Pru1Init(pru_mem_t* mem) {
 
   pam1 = PamInitMuscle(PressureSensorInit(mux,6,0x28), 5, 4, refract_cnts,
                         FiltIirInit(1, k_lp_1_2Hz_b, k_lp_1_2Hz_a));
-  PamSetPd(pam1,p_default);
+  PamSetPd(pam1,mem->p->p_sense);
 
   pam2 = PamInitMuscle(PressureSensorInit(mux,3,0x28),7, 6, refract_cnts,
                         FiltIirInit(1, k_lp_1_2Hz_b, k_lp_1_2Hz_a));
-  PamSetPd(pam2,p_default);
+  PamSetPd(pam2,mem->p->p_sense);
 
   thumbsup = ThumbsUpInteractionInit(pam1, pam2,
                                   FiltIirInit(1, b_mvavg, a_mvavg));
+
+  sync = SyncInitChan(sync_pin);
+  SyncOutLow(sync);
 }
 
 void Pru1UpdateState(const pru_count_t* c,
@@ -106,38 +103,12 @@ void Pru1UpdateState(const pru_count_t* c,
 
   PamUpdate(pam1);
   PamUpdate(pam2);
-
-  if (c->frame > 5000) {
-
-
-    ThumbsUpInteractionUpdate(thumbsup, 3000,
-                           0x280000, // 40 psi
-                           0x280000, // 40 psi
-                           -fix16_one,
-                           fix16_one,
-                           0xF0000);
-  }
-
-
-//  // update pd if param changed
-//  if (PruGetCtlBit(ctl_, 1)) {
-//    PamSetPd(pam1,p_->pd);
-//    PamSetPd(pam2,p_->pd);
-//    PruClearCtlBit(ctl_,1);
-//  }
-//  t = (c->frame - t0)*p_->fs_hz / (Td - Td/p_->fs_hz);
-//  fix16_t pd_ =  fix16_sadd(
-//    fix16_sdiv(fix16_from_int((int32_t)l_->lut[t % 1000]),fix16_from_int(100)),
-//    fix16_from_int(25));
-//  PamSetPd(pam1,pd_);
-//  PamSetPd(pam2,pd_);
-
-
-//
-
-
-
-
+  ThumbsUpInteractionUpdate(thumbsup, p_->hold_cnt,
+                         p_->p_sup,
+                         p_->p_pro,
+                         p_->thr_sup,
+                         p_->thr_pro,
+                         p_->p_sense);
 }
 
 void Pru1UpdateControl(const pru_count_t* c,
@@ -157,6 +128,11 @@ void Pru1UpdateControl(const pru_count_t* c,
   s_->pam2_state = PamGetState(pam2);
   s_->triggersignal = ThumbsUpInteractionGetTriggerSig(thumbsup);
   s_->thumbsfsm = ThumbsUpInteractionGetState(thumbsup);
+
+  if (PruGetCtlBit(ctl_,0))
+    SyncOutHigh(sync);
+  else
+    SyncOutLow(sync);
 }
 
 void Pru1Cleanup(void) {
