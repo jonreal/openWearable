678ea618ee99cbc65b1139eed102591ad8d7ad30
XW:beaglebone <xwang442@ucr.edu>
2023-04-21 18:33:42 +0000

    Xinyao's program with new pam.c file for new functions

--- apps/sysID-dual-actuators/format.c
+++ apps/sysID-dual-actuators/format.c
@@ -33,26 +33,56 @@ void FormatSprintParams(const param_mem_t* param, char* buff) {
 void FormatSprintState(const state_t* st, char* buff) {
   sprintf(buff,
           "%u\t"        // timeStamp - uint32_t
-          "%u\t"        // pru0 var - uint32_t
-          "%u\t"        // pru1 var - uint32_t
-          "%u\t"        // cpu var - uint32_t
+          "%f\t"        // x - fix16_t
+          "%f\t"        // motor u - fix16_t
+          "%f\t"        // motor duty - fix16_t
+          "%f\t"        // motor current - fix16_t
+          "%f\t"        // motor vel - fix16_t
+          "%f\t"        // reservior pressure - fix16_t
+          "%f\t"        // pam1 pm1 - fix16_t
+          "%f\t"        // pam1 pd1 - fix16_t
+          "%i\t"        // pam1 u1 - uint8_t
+          "%f\t"        // pam2 pm2 - fix16_t
+          "%f\t"        // pam2 pd2 - fix16_t
+          "%i\t"        // pam2 u2 - uint8_t
+          "%i\t"        // sync - uint32_t
           "\n",
           st->time,
-          st->pru0var,
-          st->pru1var,
-          st->cpudata.cpuvar
+          fix16_to_float(st->x),
+          fix16_to_float(st->motor.u),
+          fix16_to_float(st->motor.duty),
+          fix16_to_float(st->motor.current),
+          fix16_to_float(st->motor.velocity),
+          fix16_to_float(st->p_res),
+          fix16_to_float(st->pam1_state.pm),
+          fix16_to_float(st->pam1_state.pd),
+          st->pam1_state.u,
+          fix16_to_float(st->pam2_state.pm),
+          fix16_to_float(st->pam2_state.pd),
+          st->pam2_state.u,
+          st->sync
           );
 }
 
 void FormatSprintStateHeader(char* buff) {
   sprintf(buff,
           "\n# frame\t"
-          "pru0val\t"
-          "pru1val\t"
-          "cpuval\t"
+          "x\t"
+          "u\t"
+          "duty\t"
+          "current\t"
+          "velocity\t"
+          "p_res\t"
+          "p_m1\t"
+          "p_d1\t"
+          "u1\t"
+          "p_m2\t"
+          "p_d2\t"
+          "u2\t"
+          "sync\t"
           "\n");
 }
 
 void FormatSprintPublishState(const state_t* st, char* buff) {
-  FormatSprintState(st,buff);
+  FormatSprintState(st, buff);
 }
--- apps/sysID-dual-actuators/uiloop.c
+++ apps/sysID-dual-actuators/uiloop.c
@@ -9,15 +9,30 @@ extern volatile sig_atomic_t input_ready;
 void UiPrintMenu(const pru_mem_t* pru_mem) {
   printf(
   "\n\n---------------------------------------------------------------------\n"
-  "Menu: s - start log\n"
-  "      d - stop log\n"
+  " \t f0 = %3.2f,\t f1 = %3.2f,\t A = %3.2f,\n"
+  " \t Pd1 = %3.2f,\t Pd2 = %3.2f,\t Step = %3.2f, Repeat = %3.2f \n\n"
+  "Menu: s - Start trial\n"
+  "      i - change initial frequency\n"
+  "      f - change final frequency\n"
+  "      a - change amplitude\n"
+  "      m - change desired pressure 1\n"
+  "      n - change desired pressure 2\n"
+  "      c - change frequency step\n"
+  "      r - change repetition\n"
   "      e - exit\n"
-  "-----------------------------------------------------------------------\n");
-  fflush(stdout);
+  "-----------------------------------------------------------------------\n",
+  fix16_to_float(pru_mem->p->f0),
+  fix16_to_float(pru_mem->p->f1),
+  fix16_to_float(pru_mem->p->A),
+  fix16_to_float(pru_mem->p->Pd1),
+  fix16_to_float(pru_mem->p->Pd2),
+  fix16_to_float(pru_mem->p->step),
+  fix16_to_float(pru_mem->p->repeat));
 }
 
 int UiLoop(const pru_mem_t* pru_mem) {
   char input_char = 0;
+  float input_float = 0;
   char input_string[256] = {0};
   char log_file[256] = "datalog/";
 
@@ -40,7 +55,7 @@ int UiLoop(const pru_mem_t* pru_mem) {
           return 1;
         }
 
-        // ---- Start data collection -----------------------------------------
+        // ---- Collect trial -------------------------------------------------
         case 's' : {
           printf("\t\tEnter trial name: ");
           fflush(stdout);
@@ -53,27 +68,118 @@ int UiLoop(const pru_mem_t* pru_mem) {
           UiNewLogFile(log_file);
 
           // Wait for user input to start saving data
-          printf("\t\tPress enter to start collection...\n");
+          printf("\t\tPress enter to start trial...\n");
           fflush(stdout);
           UiPollForUserInput();
           scanf(" %c", &input_char);
 
+          // Wait for enter to stop collection
+          printf("\t\tTrial ongoing...\n");
+          fflush(stdout);
+
           // Data collection loop
           UiStartLog();
+          UiSetPruCtlBit(pru_mem, 0);
 
+          // Poll for PRU done
+          UiPollPruCtlBit(pru_mem, 0, 0);
+          UiStopAndSaveLog();
+
+          log_file[0] = '\0';
+          strcat(log_file, "datalog/");
           UiPrintMenu(pru_mem);
           break;
         }
 
-        // ---- Stop data collection -----------------------------------------
-        case 'd' : {
-          if (!UiLogging())
-            printf("\t\t Not currently logging data!\n");
-          else
-            UiStopAndSaveLog();
+        // ---- start freq -------------------------------------------------
+        case 'i' : {
+          printf("\t\tEnter new start frequency: ");
+          fflush(stdout);
 
-          log_file[0] = '\0';
-          strcat(log_file, "datalog/");
+          // Wait for input.
+          UiPollForUserInput();
+          scanf(" %f", &input_float);
+          pru_mem->p->f0 = fix16_from_float(input_float);
+          UiSetPruCtlBit(pru_mem,2);
+          UiPrintMenu(pru_mem);
+          break;
+        }
+
+        // ---- end freq -------------------------------------------------
+        case 'f' : {
+          printf("\t\tEnter new end frequency: ");
+          fflush(stdout);
+
+          // Wait for input.
+          UiPollForUserInput();
+          scanf(" %f", &input_float);
+          pru_mem->p->f1 = fix16_from_float(input_float);
+          UiSetPruCtlBit(pru_mem,2);
+          UiPrintMenu(pru_mem);
+          break;
+        }
+
+        // ---- amplitude -------------------------------------------------
+        case 'a' : {
+          printf("\t\tEnter new amplitude: ");
+          fflush(stdout);
+
+          // Wait for input.
+          UiPollForUserInput();
+          scanf(" %f", &input_float);
+          pru_mem->p->A = fix16_from_float(input_float);
+          UiPrintMenu(pru_mem);
+          break;
+        }
+
+        // ---- change Pd1 -------------------------------------------------
+        case 'm' : {
+          printf("\t\tEnter new desired pressure 1: ");
+          fflush(stdout);
+
+          // Wait for input.
+          UiPollForUserInput();
+          scanf(" %f", &input_float);
+          pru_mem->p->Pd1 = fix16_from_float(input_float);
+          UiPrintMenu(pru_mem);
+          break;
+        }
+
+        // ---- change Pd2 -------------------------------------------------
+        case 'n' : {
+          printf("\t\tEnter new desired pressure 2: ");
+          fflush(stdout);
+
+          // Wait for input.
+          UiPollForUserInput();
+          scanf(" %f", &input_float);
+          pru_mem->p->Pd2 = fix16_from_float(input_float);
+          UiPrintMenu(pru_mem);
+          break;
+        }
+
+        // ---- change cycle -------------------------------------------------
+        case 'c' : {
+          printf("\t\tEnter new frequency step: ");
+          fflush(stdout);
+
+          // Wait for input.
+          UiPollForUserInput();
+          scanf(" %f", &input_float);
+          pru_mem->p->step = fix16_from_float(input_float);
+          UiPrintMenu(pru_mem);
+          break;
+        }
+
+        // ---- change repatition -------------------------------------------------
+        case 'r' : {
+          printf("\t\tEnter new repetiton: ");
+          fflush(stdout);
+
+          // Wait for input.
+          UiPollForUserInput();
+          scanf(" %f", &input_float);
+          pru_mem->p->repeat = fix16_from_float(input_float);
           UiPrintMenu(pru_mem);
           break;
         }
@@ -88,7 +194,15 @@ int PruLoadParams(const char* file, param_mem_t* param) {
   param->fs_hz = 1000;
   param->fs_ticks = HZ_TO_TICKS(param->fs_hz);
 
+  // App specific
+//  param->Tf = 20000;
+  param->f0 = 0x1999;
+  param->f1 = fix16_one;
+  param->A = 0x10000;
+  param->Pd1 = 0x1E0000;
+  param->Pd2 = 0x1E0000;
+  param->step = 0x00CCC;
+  param->repeat = 0xA0000;
+
   return 0;
 }
-
-
