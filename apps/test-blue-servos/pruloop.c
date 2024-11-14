44b3b37aea57f0cab9df41e1d8d5ec23ab22c7cf
JR:boneblue-0 <jdrealmuto@gmail.com>
2024-11-14 18:46:32 +0000

    Servo tests for blue

---
+++ apps/test-blue-servos/pruloop.c
@@ -0,0 +1,84 @@
+/* Copyright 2017-2019 Jonathan Realmuto
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+=============================================================================*/
+
+#include "pruloop.h"
+
+volatile register uint32_t __R30;
+volatile register uint32_t __R31;
+
+// pr1_pru1_pru_r30_8 - SVO1
+const uint8_t pam1_h_pin = 8;
+
+// pr1_pru1_pru_r30_10 - SVO2
+const uint8_t pam1_l_pin = 10;
+
+// gpio2[16] - SERVO_EN
+
+// ---------------------------------------------------------------------------
+// PRU0
+//
+// Edit user defined functions below
+// ---------------------------------------------------------------------------
+void Pru0Init(pru_mem_t* mem) {
+}
+
+void Pru0UpdateState(const pru_count_t* c,
+                     const param_mem_t* p_,
+                     const lut_mem_t* l_,
+                     state_t* s_,
+                     pru_ctl_t* ctl_) {
+}
+
+void Pru0UpdateControl(const pru_count_t* c,
+                       const param_mem_t* p_,
+                       const lut_mem_t* l_,
+                       state_t* s_,
+                       pru_ctl_t* ctl_){
+}
+
+void Pru0Cleanup(void) {
+}
+
+// ---------------------------------------------------------------------------
+// PRU1
+//
+// Edit user defined functions below
+// ---------------------------------------------------------------------------
+void Pru1Init(pru_mem_t* mem) {
+}
+
+void Pru1UpdateState(const pru_count_t* c,
+                     const param_mem_t* p_,
+                     const lut_mem_t* l_,
+                     state_t* s_,
+                     pru_ctl_t* ctl_) {
+
+  // Pulse a few pins
+  if ((c->frame % 1000) == 0) {
+    __R30 ^= (1 << pam1_l_pin);
+    __R30 ^= (1 << pam1_h_pin);
+  }
+}
+
+void Pru1UpdateControl(const pru_count_t* c,
+                       const param_mem_t* p_,
+                       const lut_mem_t* l_,
+                       state_t* s_,
+                       pru_ctl_t* ctl_) {
+}
+
+void Pru1Cleanup(void) {
+}
+
