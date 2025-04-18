7f57c23ba14f6850af5c91ade8ef770d8113ccaa
JR:boneblue-0 <jdrealmuto@gmail.com>
2025-04-18 18:47:28 +0000

    load-cell-test

--- apps/load-cell-test/uiloop.c
+++ apps/load-cell-test/uiloop.c
@@ -85,7 +85,7 @@ int UiLoop(const pru_mem_t* pru_mem) {
 int PruLoadParams(const char* file, param_mem_t* param) {
 
   // Defaults
-  param->fs_hz = 100;
+  param->fs_hz = 1000;
   param->fs_ticks = HZ_TO_TICKS(param->fs_hz);
 
   return 0;
