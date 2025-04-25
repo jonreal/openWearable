856dae1618f43cd8a22c7579495b39ac9aebcf7f
JR:boneblue-0 <jdrealmuto@gmail.com>
2025-04-25 18:09:02 +0000

    Updated resource table headers, removing old things

--- library/include/rsc_table_pru1.h
+++ library/include/rsc_table_pru1.h
@@ -6,48 +6,10 @@
 #include "pru.h"
 #include "pru_virtio_ids.h"
 
-
-/* PRU Global Offsets */
-#define PRU_INTC		(PRU_GLOBAL_BASE + 0x00020000)
-#define PRU0_DEBUG		(PRU_GLOBAL_BASE + 0x00022400)
-#define PRU1_DEBUG		(PRU_GLOBAL_BASE + 0x00024400)
-#define PRU_CFG			(PRU_GLOBAL_BASE + 0x00026000)
-#define PRU_UART0		(PRU_GLOBAL_BASE + 0x00028000)
-#define PRU_IEP			(PRU_GLOBAL_BASE + 0x0002E000)
-#define PRU_ECAP0		(PRU_GLOBAL_BASE + 0x00030000)
-#define PRU0_IRAM		(PRU_GLOBAL_BASE + 0x00034000)
-#define PRU1_IRAM		(PRU_GLOBAL_BASE + 0x00038000)
-
 /* PRU Local Offsets */
-//#define PRU_IRAM		(0x00000000)
-//#define PRU_DRAM		(0x00000000)
-//#define PRU_L_SHARED_DRAM	(0x00002000)
 #define PRU_DRAM            (0x00000000)
 #define PRU_OTHER_DRAM      (0x00002000)
 #define PRU_L_SHARED_DRAM   (0x00010000)
-
-
-/* Sizes */
-#define PRU0_IRAM_SIZE		(SZ_8K)
-#define PRU1_IRAM_SIZE		(SZ_8K)
-
-#define PRU0_DRAM_SIZE		(SZ_8K)
-#define PRU1_DRAM_SIZE		(SZ_8K)
-
-#define PRU_SHARED_DRAM_SIZE	(SZ_8K + SZ_4K)
-
-/*
- * Sizes of the virtqueues (expressed in number of buffers supported,
- * and must be power of 2)
- */
-#define PRU_RPMSG_VQ0_SIZE	2
-#define PRU_RPMSG_VQ1_SIZE	2
-
-/* flip up bits whose indices represent features we support */
-#define RPMSG_PRU_C0_FEATURES	1
-
-/* Definition for unused interrupts */
-#define HOST_UNUSED		255
 
 struct my_resource_table {
 	struct resource_table base;
@@ -55,9 +17,9 @@ struct my_resource_table {
 
 };
 
-#pragma DATA_SECTION(am335x_pru_remoteproc_ResourceTable, ".resource_table")
-#pragma RETAIN(am335x_pru_remoteproc_ResourceTable)
-struct my_resource_table am335x_pru_remoteproc_ResourceTable = {
+#pragma DATA_SECTION(resourceTable, ".resource_table")
+#pragma RETAIN(resourceTable)
+struct my_resource_table resourceTable = {
 	1,	/* we're the first version that implements this */
 	0,	/* number of entries in the table */
 	0, 0,	/* reserved, must be zero */
