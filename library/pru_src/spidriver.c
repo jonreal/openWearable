442e51ff190d8270b55094036f97490f5505610f
JR:boneblue-0 <jdrealmuto@gmail.com>
2025-07-31 18:50:04 +0000

    spi bus for ads131 -- not yet working

--- library/pru_src/spidriver.c
+++ library/pru_src/spidriver.c
@@ -22,7 +22,7 @@
 void spiInit(void)
 {
 
-  uint16_t clkdiv_ch0 = 800;
+  uint16_t clkdiv_ch0 = 400;
   uint16_t clkdiv_ch1 = 200;
 
   // CM_PER_SPI1_CLKCTRL: MODULEMODE = 0x2
@@ -56,25 +56,34 @@ void spiInit(void)
   HWREG(SOC_SPI_1_REGS + 0x11C) = 0x00000000;
 
   // -- CHANNEL 0 -------------------------------------------------------------
-  //
-  // Set up for LSM6DS3.
-  // Word Length - 16-bit
-  // POL = 1, PHA = 1
   // --------------------------------------------------------------------------
 
-  // MCSPI_CH0CONF:   CLKG = 0x1 - a clock cycle granularity
-  //                  DPE0 = 0x1 - No TX on SPIDAT[0]
-  //                  WL = 0x7 - word length of 8-bits
-  //                  EPOL = 0x1 - CS low for active state
-  //                  CLKD = 0x4 - divide by 16
-  HWREG(SOC_SPI_1_REGS + 0x12C) = (0x1 << 29) | (0x0 << 25) | (0x0 << 18)
-                                | (0x1 << 16)
-                                | (0x17 << 7) | (0x1 << 6)
-                                | ( (clkdiv_ch0 & 0xF) << 2) | (0x1);
+  // MCSPI_CH0CONF:
+  HWREG(SOC_SPI_1_REGS + 0x12C) =
+      (0x1 << 29)                 // CLKG, Clock div granularity
+    | (0x1 << 28)                 // FFER, FIFO Rx enable
+    | (0x1 << 27)                 // FFEW, FIFO Tx enable
+    | (0x0 << 25)                 // TCS, Chip select time control
+    | (0x0 << 24)                 // SBPOL, Start bit polarity
+    | (0x0 << 23)                 // SBE, Start bit enable
+    | (0x0 << 21)                 // SPIENSLV,
+    | (0x0 << 20)                 // Force, Manual assert SPIEN
+    | (0x0 << 19)                 // Turbo,
+    | (0x0 << 18)                 // IS, Input select
+    | (0x0 << 17)                 // DPE1, Tx enable on SPIDAT[1]
+    | (0x1 << 16)                 // DPE0, Tx enable on SPIDAT[0]
+    | (0x0 << 15)                 // DMAR, DMA read request
+    | (0x0 << 14)                 // DMAW, DMA write request
+    | (0x0 << 12)                 // TRM, Tx/RX modes
+    | (0x17 << 7)                 // WL, word length
+    | (0x1 << 6)                  // EPOL, SPIEN polarity
+    | ( (clkdiv_ch0 & 0xF) << 2)  // CLKD, Freq div
+    | (0x0 << 1)                  // POL
+    | (0x1);                      // PHA
 
-  // MCSPI_CH0CTRL:   EXTCLK =  clock divider extension
-  //                  EN = 0x1 - enable channel 0
-  HWREG(SOC_SPI_1_REGS + 0x134) = ( ((clkdiv_ch0 >> 4) & 0xFF) << 8) | 0x1;
+  // MCSPI_CH0CTRL:  EN = 0x1 - enable channel 0
+ // HWREG(SOC_SPI_1_REGS + 0x134) =  0x1;
+  HWREG(SOC_SPI_1_REGS + 0x134) = ( ((clkdiv_ch1 >> 4) & 0xFF) << 8) | 0x1;
 
   // -- CHANNEL 1 -------------------------------------------------------------
   //
@@ -88,13 +97,13 @@ void spiInit(void)
   //                  WL = 0x7 - word length of 8-bits
   //                  EPOL = 0x1 - CS low for active state
   //                  CLKD = 0x4 - divide by 16
-  HWREG(SOC_SPI_1_REGS + 0x140) = (0x1 << 29) | (0x3 << 25) | (0x1 << 16)
-                                | (0x7 << 7) | (0x1 << 6)
-                                | ( (clkdiv_ch1 & 0xF) << 2);
+  //HWREG(SOC_SPI_1_REGS + 0x140) = (0x1 << 29) | (0x3 << 25) | (0x1 << 16)
+  //                              | (0x7 << 7) | (0x1 << 6)
+  //                              | ( (clkdiv_ch1 & 0xF) << 2);
 
-  // MCSPI_CH1CTRL:   EXTCLK =  clock divider extension
-  //                  EN = 0x1 - enable channel 1
-  HWREG(SOC_SPI_1_REGS + 0x148) = ( ((clkdiv_ch1 >> 4) & 0xFF) << 8) | 0x1;
+  //// MCSPI_CH1CTRL:   EXTCLK =  clock divider extension
+  ////                  EN = 0x1 - enable channel 1
+  //HWREG(SOC_SPI_1_REGS + 0x148) = ( ((clkdiv_ch1 >> 4) & 0xFF) << 8) | 0x1;
 
 }
 
@@ -138,3 +147,12 @@ void spiCleanUp(void)
   HWREG(0x44E00050) = (0x0);
 }
 
+void spiForceBeginXfer() {
+  HWREG(SOC_SPI_1_REGS + 0x12C) |= (1 << 20);  // FORCE=1
+  __delay_cycles(10);
+}
+
+void spiForceEndXfer() {
+  HWREG(SOC_SPI_1_REGS + 0x12C) &= ~(1 << 20);
+  __delay_cycles(10);
+}
