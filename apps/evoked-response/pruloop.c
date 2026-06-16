fbb2a1a8c21a2dea77a05b8073a6a42040648797
jdrealmuto@gmail.com <jrealmut@ucr.edu>
2026-06-16 17:26:16 +0000

    Added evoked-response app

---
+++ apps/evoked-response/pruloop.c
@@ -0,0 +1,90 @@
+/* Copyright 2017-2019 Jonathan Realmuto
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+=============================================================================*/
+
+#include "pruloop.h"
+#include "hscsann150pg2a3.h"
+#include "sync.h"
+
+volatile register uint32_t __R30;
+volatile register uint32_t __R31;
+
+// Pam
+i2cmux_t* mux;
+i2c_t* i2c;
+pressure_sensor_t* pressure_sensor;
+
+// Sync In
+sync_t* sync;
+
+uint32_t log_flag = 0;
+
+// ---------------------------------------------------------------------------
+// PRU0
+//
+// Edit user defined functions below
+// ---------------------------------------------------------------------------
+void Pru0Init(pru_mem_t* mem) {
+}
+
+void Pru0UpdateState(const pru_view_t* view, pru_io_t* io) {
+}
+
+void Pru0UpdateControl(const pru_view_t* view, pru_io_t* io) {
+}
+
+void Pru0Cleanup(void) {
+}
+// ---------------------------------------------------------------------------
+// PRU1
+//
+// Edit user defined functions below
+// ---------------------------------------------------------------------------
+void Pru1Init(pru_mem_t* mem) {
+
+  i2c = I2cInit(2);
+  mux = MuxI2cInit(i2c,0x70,PCA9548);
+
+  // pam -- only sensor for now
+  // sensor on mux ch. 6, 6 - 1 = 5
+  pressure_sensor = PressureSensorInit(mux,5,0x28);
+
+  // Sync Out ---- NEED TO CHANGE TO PROPER PIN
+  // Out: P8.44, MODE5, pr1_pru1_pru_r30_3 */
+  sync = SyncInitChan(0);
+
+}
+
+void Pru1UpdateState(const pru_view_t* view, pru_io_t* io) {
+
+  uint32_t ctl_bit = PruGetCtlBit(io->ctl,0);
+  if (ctl_bit && !log_flag) {
+    log_flag = 1;
+    SyncOutHigh(sync);
+  } else if (log_flag && !ctl_bit) {
+    log_flag = 0;
+    SyncOutLow(sync);
+  }
+
+  io->s->pressure = PressureSensorSample(pressure_sensor);
+  io->s->sync_state = SyncGetState(sync);
+}
+
+void Pru1UpdateControl(const pru_view_t* view, pru_io_t* io) {
+}
+
+void Pru1Cleanup(void) {
+  PressureSensorFree(pressure_sensor);
+}
+
---
+++ apps/evoked-response/state.h
@@ -0,0 +1,31 @@
+#ifndef _STATE_
+#define _STATE_
+
+#include "fix16.h"
+#include "../../library/pru/include/pam.h"
+
+// packing can be issue -- use 32bit types
+
+
+// --- cpudata struct
+typedef struct {
+  volatile uint32_t cpuvar;
+} cpudata_t;
+
+// --- State
+typedef struct {
+  volatile uint32_t frame;
+  cpudata_t cpudata;
+  volatile fix16_t pressure;
+  volatile uint32_t sync_state;
+ } state_t;
+
+// --- Parameters
+typedef struct {
+  volatile uint32_t debug_buff[10];
+  uint32_t fs_hz;
+  uint32_t fs_ticks;
+} param_mem_t;
+
+
+#endif
